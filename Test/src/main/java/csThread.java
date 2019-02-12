2ba17d2db9a4fcd40380139a323a6ad750b5f933
zlf <3476293819@qq.com>
2019-02-12 18:02:18 +0800

    测试

---
+++ Test/src/main/java/csThread.java
@@ -0,0 +1,16 @@
+public class csThread {
+
+    public static void main(String[] args) {
+
+        Thread t = new Thread(){
+            @Override
+            public void run(){
+
+
+            }
+
+
+        };
+
+    }
+}
---
+++ dataStructure/src/main/java/queue/queueTest.java
@@ -0,0 +1,12 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class queueTest {
+
+    public static void main(String[] args) {
+        Queue q = new LinkedList();
+    }
+
+ }
