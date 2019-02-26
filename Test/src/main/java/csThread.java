50f824bae8b5ee6bd4334393092e437e674b54d8
zlf <3476293819@qq.com>
2019-02-26 18:05:10 +0800

    cs

--- Test/src/main/java/csThread.java
+++ Test/src/main/java/csThread.java
@@ -1,16 +1,22 @@
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class csThread {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        //RandomAccessFile cs = new RandomAccessFile("d:/cx.txt","rw");
 
-        Thread t = new Thread(){
-            @Override
-            public void run(){
+        Map m = new ConcurrentHashMap();
+
+        int a = true ? 1:0;
+        System.out.println(a);
 
 
-            }
-
-
-        };
 
     }
 }
---
+++ algorithmDesign/src/main/java/sort/quickSort.java
@@ -0,0 +1,9 @@
+package sort;
+
+public class quickSort {
+    public void sort(int[] a ,int left, int right){
+
+
+
+    }
+}
